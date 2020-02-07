9be71a8a1a50d66f84440ded67e2409f781e4baa
Miguel Domingues <miguel.domingues@petrotec.com>
2020-02-07 15:27:49 +0000

    Added the interface to configurate dynamic columns and support to hidde columns

---
+++ src/app/grid1/models/column-Config.ts
@@ -0,0 +1,15 @@
+// tslint:disable-next-line:class-name
+export interface columnConfig {
+
+    field: string;
+    header: string;
+    dataType: string;
+    sortable: boolean;
+    resizable: boolean;
+    filterable: boolean;
+    movable: boolean;
+    pinned: boolean;
+    groupable: boolean;
+    hidden: boolean;
+
+}
